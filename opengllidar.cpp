cae8df1135b1f087aea217b874bdf2681a97ea62
JucovAllexandr <sania337@gamil.com>
2019-09-11 15:31:29 +0300

    bug fixed

---
+++ opengllidar.cpp
@@ -0,0 +1,6 @@
+#include "opengllidar.h"
+
+OpenGLLIDAR::OpenGLLIDAR()
+{
+
+}
---
+++ opengllidar.h
@@ -0,0 +1,11 @@
+#ifndef OPENGLLIDAR_H
+#define OPENGLLIDAR_H
+
+
+class OpenGLLIDAR
+{
+public:
+    OpenGLLIDAR();
+};
+
+#endif // OPENGLLIDAR_H
